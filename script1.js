90d57bac5b44360367115b4c6d3f52c5208c608e
Nikita <draco1997pst@gmail.com>
2024-01-18 23:42:39 +0200

    first commit

---
+++ script1.js
@@ -0,0 +1,11 @@
+let a = prompt("entet num 1 ")
+let b = prompt("enter num 2")
+let sum = 0;
+
+if (b > a){
+for (let i = a; i < b; i++){
+    sum += parseInt(i);
+}}
+else{console.error("сначала меньшее число потом большое ")}
+
+console.log("Сумма диапазона "+ sum)
